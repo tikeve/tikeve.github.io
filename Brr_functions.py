ed36322578e6e764036c51fa34298534899ab57c
tikeve <56687625+tikeve@users.noreply.github.com>
2020-11-23 11:38:03 +0300

    ddd

--- Brr_functions.py
+++ Brr_functions.py
@@ -40,14 +40,14 @@ def no_lists(t):
                     table.at[i,j]=table.at[i,j][0]
                 elif  len(table.at[i,j])==2:
                     if j+'*' not in table.columns:
-                        table.insert(list(table.columns).index(j),j+'*',''*len(table))
+                        table.insert(list(table.columns).index(j),j+'*',np.nan)
                     table.at[i,j+'*'] = table.at[i,j][1]
                     table.at[i,j]=table.at[i,j][0]
                 else:#len(table.at[i,j])==3
                     if j+'*' not in table.columns:
-                        table.insert(list(table.columns).index(j),j+'*',''*len(table))
+                        table.insert(list(table.columns).index(j),j+'*',np.nan)
                     if j+'**' not in table.columns:
-                        table.insert(list(table.columns).index(j)+1,j+'**',''*len(table))
+                        table.insert(list(table.columns).index(j)+1,j+'**',np.nan)
                     table.at[i,j+'**'] = table.at[i,j][2]
                     table.at[i,j+'*'] = table.at[i,j][1]
                     table.at[i,j]=table.at[i,j][0]
