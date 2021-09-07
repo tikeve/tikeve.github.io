ff65fa886898cdc21fa867a6e4018b0e71dc0452
tikeve <56687625+tikeve@users.noreply.github.com>
2021-09-07 23:35:04 +0300

    asdasd

---
+++ html/Script.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+var X = '<form action="index.html" ><input type="submit" class="history_button" value="2021 /         " /></form><form action="html/2020_2021/index.html"><input type="submit" class="history_button" value="2020 / 2021" /></form><form action="html/2019_2020/index.html"><input type="submit" class="history_button" value="2019 / 2020" /></form>';
+//document.getElementById("FromScript").innerHTML = 3;
+
+
+
+	//<form action = "index.html">
+	//	<input type="submit" class="history_button" value="2021 /     " />
+	//</form>
+	//<form action="html/2020_2021/index.html">
+	//	<input type="submit" class="history_button" value="2020 / 2021" />
+	//</form>
+	//<form action="html/2019_2020/index.html">
+	//	<input type="submit" class="history_button" value="2019 / 2020" />
+	//</form>
+
+console.log(X);
